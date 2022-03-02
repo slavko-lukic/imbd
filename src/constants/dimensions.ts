c6af39ad68839c0566ef895547eb91cd60f11730
Slavko Lukić <87854709+slavko-lukic@users.noreply.github.com>
2022-03-02 10:16:20 +0100

    Added settings button to header

--- src/constants/dimensions.ts
+++ src/constants/dimensions.ts
@@ -1,1 +1,2 @@
 export const HEADER_HEIGHT = 60;
+export const HEADER_ICON_SIZE = 30;
--- src/screens/MoviesScreen.tsx
+++ src/screens/MoviesScreen.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useCallback, useState} from 'react';
 import {ListRenderItem} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import MainHeader from '../components/MainHeader';
@@ -9,6 +9,8 @@ import {SUGGESTED_MOVIES, WATCHED_MOVIES, WATCHLIST} from '../mock/movies_mock';
 import {Movie} from '../models/movie';
 import {FlatList} from 'react-native-gesture-handler';
 import MovieListSelectorButton from '../components/MovieListSelectorButton';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {HEADER_ICON_SIZE} from '../constants/dimensions';
 
 const MoviesScreen = () => {
   const [listData, setListData] = useState<Movie[]>(SUGGESTED_MOVIES);
@@ -17,7 +19,7 @@ const MoviesScreen = () => {
     <MovieCard movie={item} index={index} />
   );
 
-  const listDataUpdateHandler = (displayedList: MovieListTypes) => {
+  const listDataUpdateHandler = useCallback((displayedList: MovieListTypes) => {
     switch (displayedList) {
       case MovieListTypes.WATCHLIST:
         setListData(WATCHLIST);
@@ -32,16 +34,29 @@ const MoviesScreen = () => {
         setListData(SUGGESTED_MOVIES);
         break;
     }
-  };
+  }, []);
+
+  const goToSettings = () => {};
+
+  const headerLeftButton: JSX.Element = (
+    <MovieListSelectorButton updateListData={listDataUpdateHandler} />
+  );
+  const headerRightButton: JSX.Element = (
+    <Ionicons
+      name="settings-sharp"
+      color={colors.WHITE}
+      size={HEADER_ICON_SIZE}
+      onPress={goToSettings}
+    />
+  );
 
   return (
     <SafeAreaView
       edges={['top']}
       style={{backgroundColor: colors.BACKGROUND, flex: 1, height: '100%'}}>
       <MainHeader
-        leftButton={
-          <MovieListSelectorButton updateListData={listDataUpdateHandler} />
-        }
+        leftButton={headerLeftButton}
+        rightButton={headerRightButton}
       />
       <FlatList data={listData} renderItem={renderItem} />
     </SafeAreaView>
