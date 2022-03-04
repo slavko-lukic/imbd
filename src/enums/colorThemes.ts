28af43c6b9ed3830e672d02959c8331fad8497ed
Slavko Lukić <87854709+slavko-lukic@users.noreply.github.com>
2022-03-04 12:01:55 +0100

    Removed unused theme names

--- src/enums/colorThemes.ts
+++ src/enums/colorThemes.ts
@@ -1,9 +1,5 @@
 export enum ColorThemes {
   CLASSIC_DARK = 'Classic Dark',
-  DRACULA = 'Dracula',
   NORDIC = 'Nordic',
-  VEGA_IT = 'Vega IT',
-
-  CLASSIC_LIGHT = 'Classic Light',
   SOLARIZED = 'Solarized',
 }
