3e3358d083f32757c7ab7732308a43876ee7131a
Slavko Lukić <87854709+slavkolukic@users.noreply.github.com>
2022-03-16 16:38:25 +0100

    Added properties and shadow to bouncy heart switch

--- src/components/BouncyHeartSwitch.tsx
+++ src/components/BouncyHeartSwitch.tsx
@@ -1,4 +1,4 @@
-import Svg, {Circle, Path} from 'react-native-svg';
+import Svg, {Circle, Defs, Path, RadialGradient, Stop} from 'react-native-svg';
 import React, {FC, useEffect, useState} from 'react';
 import Animated, {
   interpolateColor,
@@ -11,22 +11,32 @@ import Animated, {
   WithSpringConfig,
   withTiming,
 } from 'react-native-reanimated';
+import {StyleProp, ViewStyle} from 'react-native';
 
 const radius = 7;
-
-const ACTIVE_FILL = '#ff708f';
-const ACTIVE_OUTLINE = '#ff4e74';
-const INACTIVE_FILL = '#ffb3c3';
-const INACTIVE_OUTLINE = '#ff809b';
 
 const AniamtedCircle = Animated.createAnimatedComponent(Circle);
 const AnimatedPath = Animated.createAnimatedComponent(Path);
 
 interface BouncyHeartSwitchProps {
   size?: number;
+  style?: StyleProp<ViewStyle>;
+  activeFillColor?: string;
+  activeOutlineColor?: string;
+  inactiveFillColor?: string;
+  inactiveOutlineColor?: string;
+  circleFillColor?: string;
 }
 
-const BouncyHeartSwitch: FC<BouncyHeartSwitchProps> = ({size = 50}) => {
+const BouncyHeartSwitch: FC<BouncyHeartSwitchProps> = ({
+  size = 60,
+  style,
+  activeFillColor = '#ff708f',
+  activeOutlineColor = '#ff4e74',
+  inactiveFillColor = '#fff',
+  inactiveOutlineColor = '#ff809b',
+  circleFillColor = '#fff',
+}) => {
   const [switchState, setSwitchState] = useState(false);
 
   const positionX = useSharedValue(0);
@@ -93,32 +103,62 @@ const BouncyHeartSwitch: FC<BouncyHeartSwitchProps> = ({size = 50}) => {
 
   const animatedProps = useAnimatedProps(() => {
     return {
-      fill: interpolateColor(color.value, [0, 1], [INACTIVE_FILL, ACTIVE_FILL]),
+      fill: interpolateColor(
+        color.value,
+        [0, 1],
+        [inactiveFillColor, activeFillColor],
+      ),
       stroke: interpolateColor(
         color.value,
         [0, 1],
-        [INACTIVE_OUTLINE, ACTIVE_OUTLINE],
+        [inactiveOutlineColor, activeOutlineColor],
       ),
     };
   }, []);
 
   return (
     <Svg
+      style={style}
       onPress={onPressHandler}
       viewBox="-2 0 37 23"
       width={size}
       height={size * 0.7}>
+      {/* Heart background */}
       <AnimatedPath
         strokeWidth={2}
         d="M23.5,0.5 C28.4705627,0.5 32.5,4.52943725 32.5,9.5 C32.5,16.9484448 21.46672,22.5 16.5,22.5 C11.53328,22.5 0.5,16.9484448 0.5,9.5 C0.5,4.52952206 4.52943725,0.5 9.5,0.5 C12.3277083,0.5 14.8508336,1.80407476 16.5007741,3.84362242 C18.1491664,1.80407476 20.6722917,0.5 23.5,0.5 Z"
         animatedProps={animatedProps}
       />
+
+      {/* Gradient used for shadow */}
+      <Defs>
+        <RadialGradient
+          id="grad"
+          cx="13"
+          cy="0"
+          r={radius + 1}
+          gradientUnits="userSpaceOnUse">
+          <Stop offset="0" stopColor="black" stopOpacity="0.9" />
+          <Stop offset="1" stopColor="black" stopOpacity="0" />
+        </RadialGradient>
+      </Defs>
+
+      {/* Shadow  */}
       <AniamtedCircle
         cx={13}
-        translateX={0}
-        r={radius}
+        cy={0}
+        r={radius + 1}
         style={[animatedStyle]}
-        fill="white"
+        fill="url(#grad)"
+      />
+
+      {/* Circle */}
+      <AniamtedCircle
+        cx={13}
+        cy={0}
+        r={radius - 1}
+        style={[animatedStyle]}
+        fill={circleFillColor}
       />
     </Svg>
   );
