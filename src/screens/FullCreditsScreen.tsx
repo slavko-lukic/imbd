303738e10947b723db085bd812c3a84290638ad9
Slavko Lukić <87854709+slavkolukic@users.noreply.github.com>
2022-03-18 14:07:34 +0100

    Added mini header component

--- src/screens/FullCreditsScreen.tsx
+++ src/screens/FullCreditsScreen.tsx
@@ -1,9 +1,12 @@
 import {StackScreenProps} from '@react-navigation/stack';
 import React, {FC} from 'react';
-import {StyleSheet, ListRenderItem, View} from 'react-native';
+import {StyleSheet, ListRenderItem, View, Text} from 'react-native';
 import {FlatList} from 'react-native-gesture-handler';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MiniHeader from '../components/MiniHeader';
 import MovieCreditCard from '../components/MovieCreditCard';
 import VerticalSpacing from '../components/VerticalSpacing';
+import {HEADER_ICON_SIZE} from '../constants/dimensions';
 import {cardOnlyTopShadow} from '../constants/styling';
 import {AppRoute} from '../enums/routes';
 import {useColorTheme} from '../hooks/styles/useColorTheme';
@@ -18,13 +21,18 @@ type FullCreditsScreenProps = StackScreenProps<
 
 const FullCreditsScreen: FC<FullCreditsScreenProps> = ({route, navigation}) => {
   const dataSource = route.params;
-  const {surfaceStyle} = useColorTheme();
+  const {surfaceStyle, colorTheme, primaryVariantColorForegroundStyle} =
+    useColorTheme();
 
   const getRole = (credit: Cast | Crew): string => {
     if ('job' in credit) {
       return credit.job;
     }
     return credit.character;
+  };
+
+  const goBack = () => {
+    navigation.goBack();
   };
 
   const renderItem: ListRenderItem<Cast | Crew> = ({item}) => (
@@ -40,15 +48,36 @@ const FullCreditsScreen: FC<FullCreditsScreenProps> = ({route, navigation}) => {
     return item.credit_id + getRole(item);
   };
 
+  const headerLeftButton: JSX.Element = (
+    <Ionicons
+      name="close"
+      color={colorTheme.foreground}
+      size={HEADER_ICON_SIZE}
+      onPress={goBack}
+    />
+  );
+
+  const headerMiddleElement: JSX.Element = (
+    <Text
+      numberOfLines={1}
+      style={[{fontSize: 17}, primaryVariantColorForegroundStyle]}>
+      Cast ({dataSource.length})
+    </Text>
+  );
+
   return (
     <View style={[styles.container, cardOnlyTopShadow, surfaceStyle]}>
+      <MiniHeader
+        middleElement={headerMiddleElement}
+        leftButton={headerLeftButton}
+      />
       <FlatList
         style={styles.list}
         data={dataSource as Array<Cast | Crew>}
         renderItem={renderItem}
         keyExtractor={keyExtractor}
         ListFooterComponent={<VerticalSpacing spacing={60} />}
-        ListHeaderComponent={<VerticalSpacing spacing={30} />}
+        ListHeaderComponent={<VerticalSpacing spacing={20} />}
       />
     </View>
   );
