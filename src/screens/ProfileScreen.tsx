169d4bf91202e1d6e26f8f5acd362765153d13ef
Slavko Lukić <87854709+slavkolukic@users.noreply.github.com>
2022-03-24 12:26:13 +0100

    Added svg flower animation

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,27 +1,38 @@
-import React, {FC, useState} from 'react';
-import {Slider, StyleSheet, Text, View} from 'react-native';
+import React, {FC, useEffect} from 'react';
+import {StyleSheet, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import MainHeader from '../components/MainHeader';
 import {AppRoute} from '../enums/routes';
 import {BottomTabScreenProps} from '@react-navigation/bottom-tabs';
 import {BottomTabNavigatorParams} from '../navigation/BottomTabs';
 import {useColorTheme} from '../hooks/styles/useColorTheme';
-import Neumorphling from '../components/Neumorphling';
-import {solarizedThemeColors} from '../constants/colors';
+import colors from '../constants/colors';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {HEADER_ICON_SIZE} from '../constants/dimensions';
+import Svg, {Circle, G} from 'react-native-svg';
+import Animated, {
+  Extrapolate,
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withDelay,
+  withRepeat,
+  withSequence,
+  withTiming,
+} from 'react-native-reanimated';
 
 type ProfileScreenProps = BottomTabScreenProps<
   BottomTabNavigatorParams,
   AppRoute.PROFILE
 >;
 
+const AnimatedCircle = Animated.createAnimatedComponent(Circle);
+const AnimatedG = Animated.createAnimatedComponent(G);
+
+const SIZE = 250;
+
 const ProfileScreen: FC<ProfileScreenProps> = ({navigation}) => {
   const {colorTheme} = useColorTheme();
-
-  const [x, setX] = useState(-1);
-  const [y, setY] = useState(-1);
-  const [d, setD] = useState(0);
 
   const goToSettings = () => {
     navigation.navigate(AppRoute.SETTINGS);
@@ -36,62 +47,127 @@ const ProfileScreen: FC<ProfileScreenProps> = ({navigation}) => {
     />
   );
 
-  return (
-    <SafeAreaView
-      edges={['top']}
-      style={[
-        styles.screenContaner,
-        {backgroundColor: solarizedThemeColors.SURFACE},
-      ]}>
-      <MainHeader rightButtons={headerRightButton} />
+  const a = useSharedValue(0);
 
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <Slider
-          style={{width: 100, top: -30}}
-          maximumValue={20}
-          minimumValue={0}
-          value={d}
-          onValueChange={d => setD(d)}
-        />
-        <View
-          style={{
-            left: -50,
-            flexDirection: 'row',
-          }}>
-          <Slider
-            value={y}
-            onValueChange={y => setY(y)}
-            style={{width: 100, transform: [{rotateZ: '90deg'}], top: 30}}
-            maximumValue={1}
-            minimumValue={-1}
-          />
-          <Neumorphling
-            style={{
-              width: 250,
-              height: 200,
-              justifyContent: 'center',
-              alignItems: 'center',
-              borderRadius: 20,
-            }}
-            backgroundColor={solarizedThemeColors.SURFACE}
-            lightPositionX={x}
-            lightPositionY={y}
-            distance={d}>
-            <Text>Hey</Text>
-          </Neumorphling>
-        </View>
-        <Slider
-          value={x}
-          onValueChange={x => setX(x)}
-          style={{width: 100, top: 30}}
-          maximumValue={1}
-          minimumValue={-1}
-        />
+  useEffect(() => {
+    a.value = withRepeat(
+      withSequence(
+        withDelay(500, withTiming(1, {duration: 3500})),
+        withDelay(3500, withTiming(0, {duration: 7000})),
+      ),
+      -1,
+    );
+  }, []);
+  const radius = SIZE / 4;
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {translateX: interpolate(a.value, [0, 1], [0, radius])},
+        {scale: interpolate(a.value, [0, 1], [0.25, 1])},
+      ],
+    };
+  }, []);
+
+  const animatedStyle3 = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {translateX: interpolate(a.value, [0, 1], [0, radius])},
+        {scale: interpolate(a.value, [0, 0.6], [0.25, 1], Extrapolate.CLAMP)},
+      ],
+      opacity: interpolate(a.value, [0, 1], [0, 0.5], Extrapolate.CLAMP),
+    };
+  }, []);
+
+  const animatedStyle2 = useAnimatedStyle(() => {
+    return {
+      transform: [{rotateZ: `${a.value * 90}deg`}],
+    };
+  });
+
+  return (
+    <SafeAreaView edges={['top']} style={[styles.screenContaner]}>
+      <MainHeader rightButtons={headerRightButton} />
+      <View style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
+        <Svg
+          viewBox={`${-SIZE / 2} ${-SIZE / 2} ${SIZE} ${SIZE}`}
+          width={SIZE}
+          height={SIZE}>
+          <G rotation="30">
+            <AnimatedG rotation="0" style={animatedStyle2}>
+              <AnimatedCircle
+                style={animatedStyle}
+                r={radius}
+                fill={'rgba(107,247,246,0.5)'}
+              />
+              <AnimatedCircle
+                style={animatedStyle3}
+                r={radius}
+                fill={'rgba(107,247,246,0.5)'}
+              />
+            </AnimatedG>
+            <AnimatedG rotation="60" style={animatedStyle2}>
+              <AnimatedCircle
+                style={animatedStyle}
+                r={radius}
+                fill={'rgba(91,248,237,0.5)'}
+              />
+              <AnimatedCircle
+                style={animatedStyle3}
+                r={radius}
+                fill={'rgba(91,248,237,0.5)'}
+              />
+            </AnimatedG>
+            <AnimatedG rotation="120" style={animatedStyle2}>
+              <AnimatedCircle
+                style={animatedStyle}
+                r={radius}
+                fill={'rgba(74,250,226,0.5)'}
+              />
+              <AnimatedCircle
+                style={animatedStyle3}
+                r={radius}
+                fill={'rgba(74,250,226,0.5)'}
+              />
+            </AnimatedG>
+            <AnimatedG rotation="180" style={animatedStyle2}>
+              <AnimatedCircle
+                style={animatedStyle}
+                r={radius}
+                fill={'rgba(58,251,213,0.5)'}
+              />
+              <AnimatedCircle
+                style={animatedStyle3}
+                r={radius}
+                fill={'rgba(58,251,213,0.5)'}
+              />
+            </AnimatedG>
+            <AnimatedG rotation="240" style={animatedStyle2}>
+              <AnimatedCircle
+                style={animatedStyle}
+                r={radius}
+                fill={'rgba(41,252,197, 0.5)'}
+              />
+              <AnimatedCircle
+                style={animatedStyle3}
+                r={radius}
+                fill={'rgba(41,252,197, 0.5)'}
+              />
+            </AnimatedG>
+            <AnimatedG rotation="300" style={animatedStyle2}>
+              <AnimatedCircle
+                style={animatedStyle}
+                r={radius}
+                fill={'rgba(33,253,188, 0.5)'}
+              />
+              <AnimatedCircle
+                style={animatedStyle3}
+                r={radius}
+                fill={'rgba(33,253,188, 0.5)'}
+              />
+            </AnimatedG>
+          </G>
+        </Svg>
       </View>
     </SafeAreaView>
   );
@@ -103,5 +179,6 @@ const styles = StyleSheet.create({
   screenContaner: {
     flex: 1,
     height: '100%',
+    backgroundColor: colors.BLACK,
   },
 });
