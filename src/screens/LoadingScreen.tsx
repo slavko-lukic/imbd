b10cc42b2b870b820c4953b94d3bf9560da02f48
Slavko Lukić <87854709+slavko-lukic@users.noreply.github.com>
2022-04-05 11:58:57 +0200

    Loading screen refactoring

--- src/screens/LoadingScreen.tsx
+++ src/screens/LoadingScreen.tsx
@@ -1,25 +1,14 @@
 import {StackActions} from '@react-navigation/native';
 import {StackScreenProps} from '@react-navigation/stack';
-import React, {FC, useEffect} from 'react';
-import {Alert, Image, StatusBar, StyleSheet} from 'react-native';
-import Animated, {
-  Easing,
-  useAnimatedStyle,
-  useSharedValue,
-  withDelay,
-  withRepeat,
-  withSequence,
-  withTiming,
-} from 'react-native-reanimated';
+import React, {FC, useEffect, useState} from 'react';
+import {StatusBar, StyleSheet} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {nordicThemeColors} from '../constants/colors';
 import {AppRoute} from '../enums/routes';
 import {useColorTheme} from '../hooks/styles/useColorTheme';
 import {RootStackNavigatorParams} from '../navigation/RootStackNavigator';
 import messaging from '@react-native-firebase/messaging';
-
-const logo = require('../assets/images/logo.png');
-const AnimatedImage = Animated.createAnimatedComponent(Image);
+import {FcmRequestUserPermission} from '../utilities/notifications';
+import AnimatedLoadingLogo from '../components/AnimatedLoadingLogo';
 
 type LoadingScreenProps = StackScreenProps<
   RootStackNavigatorParams,
@@ -29,81 +18,40 @@ type LoadingScreenProps = StackScreenProps<
 const LoadingScreen: FC<LoadingScreenProps> = ({navigation}) => {
   const {colorTheme, backgroundStyle} = useColorTheme();
 
-  const loadingProgress = useSharedValue(-1);
-  const bounceProgress = useSharedValue(0);
-  const opacityProgress = useSharedValue(0);
+  const [fcmAuthStatus, setFcmAuthStatus] = useState(
+    messaging.AuthorizationStatus.NOT_DETERMINED,
+  );
+  const [minimumTimePassed, setMinimumTimePassed] = useState(false);
 
+  // request user permission for notifications
   useEffect(() => {
-    requestUserPermission();
+    FcmRequestUserPermission().then(status => {
+      if (status != undefined) setFcmAuthStatus(status);
+    });
   }, []);
 
-  const requestUserPermission = async () => {
-    let authStatus = 0;
-    try {
-      authStatus = await messaging().requestPermission();
-    } catch (e: any) {
-      console.log(e.message);
-    }
-
-    Alert.alert('Authorization status:', authStatus.toString());
-  };
-
+  // set timer to measure minimum user needs to spend on loading
   useEffect(() => {
-    opacityProgress.value = withDelay(400, withTiming(1, {duration: 500}));
-    loadingProgress.value = withRepeat(
-      withSequence(
-        withTiming(1, {duration: 70, easing: Easing.bounce}),
-        withTiming(-1, {duration: 70, easing: Easing.bounce}),
-      ),
-      -1,
-    );
-    bounceProgress.value = withRepeat(
-      withSequence(
-        withTiming(1, {duration: 600, easing: Easing.bezierFn(0, 0.7, 1, 1)}),
-        withTiming(0, {duration: 600, easing: Easing.bezierFn(0.7, 0, 1, 1)}),
-      ),
-      -1,
-    );
-
-    let timer = setTimeout(
-      () => navigation.dispatch(StackActions.replace(AppRoute.HOME)),
-      3000,
-    );
-
+    let timer = setTimeout(() => setMinimumTimePassed(true), 3000);
     return () => {
       clearTimeout(timer);
     };
   }, []);
 
-  const animatedLogo = useAnimatedStyle(() => {
-    return {
-      transform: [
-        {rotateZ: loadingProgress.value * 2 + 'deg'},
-        {translateX: loadingProgress.value * 1},
-        {rotateX: loadingProgress.value * 15 + 'deg'},
-      ],
-      opacity: opacityProgress.value,
-    };
-  }, []);
+  // when conditions are met, navigate to next screen
+  useEffect(() => {
+    if (!minimumTimePassed) return;
+    if (fcmAuthStatus === messaging.AuthorizationStatus.NOT_DETERMINED) return;
 
-  const animatedCircle = useAnimatedStyle(() => {
-    return {
-      transform: [{translateY: -bounceProgress.value * 20}],
-      opacity: opacityProgress.value,
-    };
-  }, []);
+    navigation.dispatch(StackActions.replace(AppRoute.HOME));
+  }, [minimumTimePassed, fcmAuthStatus]);
 
   return (
     <SafeAreaView style={[styles.screenContaner, backgroundStyle]}>
       <StatusBar
         barStyle={colorTheme.type === 'dark' ? 'light-content' : 'dark-content'}
       />
-      <Animated.View style={[styles.circle, animatedCircle]} />
-      <AnimatedImage
-        style={[styles.image, animatedLogo]}
-        resizeMode={'cover'}
-        source={logo}
-      />
+      <AnimatedLoadingLogo />
     </SafeAreaView>
   );
 };
@@ -115,21 +63,7 @@ const styles = StyleSheet.create({
     flex: 1,
     width: '100%',
     height: '100%',
-
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  image: {
-    width: 100,
-    height: undefined,
-    aspectRatio: 1 / 1,
-
-    borderRadius: 15,
-  },
-  circle: {
-    width: 20,
-    height: 20,
-    borderRadius: 10,
-    backgroundColor: nordicThemeColors.PRIMARY,
   },
 });
