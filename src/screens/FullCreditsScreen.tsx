98be91fcf8060da692b1d66b27f65b13b3c8db44
Slavko Lukić <87854709+slavko-lukic@users.noreply.github.com>
2022-04-08 16:18:01 +0200

    Increased full credits screen top spacing

--- src/screens/FullCreditsScreen.tsx
+++ src/screens/FullCreditsScreen.tsx
@@ -130,7 +130,7 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
 
-    marginTop: 80,
+    marginTop: 100,
     borderTopLeftRadius: 15,
     borderTopRightRadius: 15,
   },
